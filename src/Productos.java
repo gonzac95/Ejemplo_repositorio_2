6bd64dd68263eb08dd13a1a53f89a2dd4680697a
tucordoba95 <castrocordoba95@gmail.com>
2022-08-18 21:20:34 -0500

    Version 2

--- src/Productos.java
+++ src/Productos.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author gocac
- */
 public class Productos {
+    
+    private String nombre;
     
 }
